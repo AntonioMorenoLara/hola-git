af3852945e6191ad80cff06275ee72e21b1dc8a5
AntonioMorenoLara <toni.ml7252@gmail.com>
2023-08-11 13:32:25 +0200

    Programa numSecreto

---
+++ numSecreto.py
@@ -0,0 +1,13 @@
+
+NUM_SECRETO = 10;
+flag = True;
+while flag:
+    print('Escribe un numero');
+    num = input();
+    if num > NUM_SECRETO:
+        print('El numero tiene que ser menor');
+    elif num < NUM_SECRETO:
+        print('El numero tiene que ser mayor');
+    else :
+        print('Enhorabuena, has acertado');
+        flag = False;
